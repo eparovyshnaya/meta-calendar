3204dc579bf389ff3e2ffc41c8731df1dc06e1c2
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-10-01 20:37:38 +0300

    document dsl

--- src/main/kotlin/ru/cleverclover/metacalendar/dsl.kt
+++ src/main/kotlin/ru/cleverclover/metacalendar/dsl.kt
@@ -1,11 +1,32 @@
 package ru.cleverclover.metacalendar
 
+/**
+ * In-here specific [scope-controlling annotation](https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-dsl-marker/index.html)
+ * */
 @DslMarker
 annotation class MetaCalendarDsl
 
+/**
+ * Use unconventionally-named object for the sake of it's
+ * [invoke operator](https://kotlinlang.org/docs/reference/operator-overloading.html#invoke).
+ *
+ * As it has the only closure argument, then we can use highly readable form
+ *
+ * ```
+ * calendar {
+ *    // init code here
+ * }
+ * ```
+ * */
 @Suppress("ClassName")
 @MetaCalendarDsl
 object calendar {
+    /**
+     * New [ConstructionContext] is created to be used as the [constructCalendar]'s
+     * [receiver](https://kotlinlang.org/docs/reference/lambdas.html#function-literals-with-receiver).
+     *
+     * Returns [MetaCalendar] instance constructed during the [constructCalendar] work.
+     * */
     operator fun invoke(constructCalendar: ConstructionContext.() -> Unit): MetaCalendar {
         val context = ConstructionContext()
         context.constructCalendar()
@@ -13,10 +34,30 @@ object calendar {
     }
 }
 
+/**
+ * Internal calendar's construction context.
+ * Creates and hosts an instance of [MetaCalendar] and exposes it's methods to fill it with data.
+ *
+ * Use it like
+ * ```
+ * calendar {
+ *      period(
+ *          from = "12 декабря",
+ *          till = "25 декабря",
+ *          note = "Йоль в Исландии"
+ *      )
+ * }
+ * ```
+ * */
 @MetaCalendarDsl
 class ConstructionContext {
     val calendar = MetaCalendar()
 
+    /**
+     * Append new period to a [MetaCalendar] under construction.
+     *
+     * From-natural-language parsing is used to recognize
+     * */
     fun period(from: String, till: String, note: Any? = null) =
             calendar.addPeriod(Period(
                     ParsedDayMark(from).mark(),

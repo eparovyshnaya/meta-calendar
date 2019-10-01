0a7f8a9fa6ceb1076b8edaf197f3509942d5f0a0
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-10-01 21:48:13 +0300

    document model

--- src/main/kotlin/ru/cleverclover/metacalendar/model.kt
+++ src/main/kotlin/ru/cleverclover/metacalendar/model.kt
@@ -4,18 +4,26 @@ import java.time.DayOfWeek
 import java.time.Month
 import java.time.ZoneId
 
+/**
+ * Model for a point on a [MetaCalendar].
+ *
+ * Extend this one if none of existing implementations fits your needs.
+ *
+ * The only thing it should do - be able to [resolve] oneself
+ * */
 abstract class DayMark {
     abstract val monthNo: Month
     abstract val note: Any?
-    // todo: no resolution facilities should reside here in model, extract to separate interfaces
+    /**
+     * Resolve the mark to a precise year and time zone: find out exact _date_
+     * */
     open fun resolve(year: Int, zone: ZoneId, startOfADay: Boolean = true) = resolvedMark(year, zone, startOfADay).date()
 
     internal abstract fun resolvedMark(year: Int, zone: ZoneId, startOfADay: Boolean): MarkResolved
 }
 
 /**
- * Like
- * | 23-rd | of september |
+ * Like *21-st of October*
  * */
 data class DayOfMonth(override val monthNo: Month,
                       val dayNo: Int,
@@ -27,9 +35,7 @@ data class DayOfMonth(override val monthNo: Month,
 }
 
 /**
- * Like
- *
- * | the second | monday | of august |
+ * Like *the second Monday in August*
  *
  * If the first week *of august* does not contain *monday*,
  * then such a tail-week is skipped as *monday*-less,
@@ -49,6 +55,9 @@ data class WeekdayInMonth(override val monthNo: Month,
     override fun toString() = "${weekday.name} #$weekNoInMonth in ${monthNo.name}"
 }
 
+/**
+ * Like *the last Wednesday in June*
+ * */
 data class LastWeekdayInMonth(override val monthNo: Month,
                               val weekday: DayOfWeek,
                               override val note: Any? = null) : DayMark() {
@@ -58,6 +67,11 @@ data class LastWeekdayInMonth(override val monthNo: Month,
     override fun toString() = "The last ${weekday.name} in ${monthNo.name}"
 }
 
+/**
+ * Like *end of May*, meaning the last day in the month.
+ *
+ * For February it means 29-th for leap years and 28-th for the rest of 'em
+ * */
 data class LastDayOfMonth(override val monthNo: Month,
                           override val note: Any? = null) : DayMark() {
     override fun resolvedMark(year: Int, zone: ZoneId, startOfADay: Boolean) =
@@ -66,10 +80,40 @@ data class LastDayOfMonth(override val monthNo: Month,
     override fun toString() = "The last day of ${monthNo.name}"
 }
 
+/**
+ * Pair of [DayMark]s, base [MetaCalendar] unit.
+ *
+ * As we do not specify *time* for a [DayMark], we use
+ *   - *LocalTime.of(0, 0, 0, 0)* for the first day mark and
+ *   - *LocalTime.of(23, 59, 59, 999)* for the end-mark
+ *
+ *   Thus, any period
+ *     - contains at least one full day (minus one nanosecond): 07.12 (start of the day) - 07.12 (end of the day)
+ *     - cannot contain more than a year (minus one nanosecond): 07.12 (start of the day) - 06.12 (end of the day)
+ *
+ * */
 data class Period(var start: DayMark, var end: DayMark, val note: Any? = null) {
+    /**
+     * Resolve a period means mostly resolving both it's ends to precise dates.
+     *
+     * If a period is crosses a year,
+     * resolution produces two periods, overlapping with the year partially.
+     *
+     *  For instance, let's resolve (10.01 - 01.01) to year of 2019. There will be
+     *   - 10.01.2018 - 01.01.2019
+     *   - 10.01.2019 - 01.01.2020
+     *
+     *  as the both are valuable for the year of 2019.
+     * */
     fun resolve(year: Int, zone: ZoneId) = PeriodResolved(this, year, zone).periods()
 }
 
+
+/**
+ * Is a simple aggregator for [Period]s, which are not ordered here.
+ *
+ * Provides bulk [resolve] for all periods on fell swoop.
+ * */
 class MetaCalendar {
     private val periods = mutableSetOf<Period>()
 
@@ -78,6 +122,9 @@ class MetaCalendar {
     fun periods(): Set<Period> = periods
     fun size() = periods.size
 
+    /**
+     * Bulk resolution for all contained periods resulting in a set of precise dates [Pair]s
+     * */
     fun resolve(year: Int, zone: ZoneId) = ResolvedCalendar(this, year, zone).dates()
 }
 

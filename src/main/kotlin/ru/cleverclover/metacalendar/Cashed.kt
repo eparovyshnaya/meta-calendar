2659ca89e97d34b1008b3199ed79e50d12610ce5
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-10-28 20:33:21 +0300

    #7 Provide license headers for the sources and manifests

--- src/main/kotlin/ru/cleverclover/metacalendar/Cashed.kt
+++ src/main/kotlin/ru/cleverclover/metacalendar/Cashed.kt
@@ -1,4 +1,17 @@
-package ru.cleverclover.metacalendar
+/*******************************************************************************
+ * Copyright (c) 2019 CleverClover
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT which is available at
+ * https://spdx.org/licenses/MIT.html#licenseText
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *     CleverClover - initial API and implementation
+ *******************************************************************************
+ */
+ package ru.cleverclover.metacalendar
 
 /**
  * Orthodox OOP construction.
